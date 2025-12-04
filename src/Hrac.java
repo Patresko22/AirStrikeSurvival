2daf85d4377e74b7993cb5fd6e08f3006a20edc3
Patresko22 <patriksamec0622@gmail.com>
2025-12-04 18:15:00 +0100

    Upravenie pohybu hráča, teraz už "nespadne" z lode

--- src/Hrac.java
+++ src/Hrac.java
@@ -3,8 +3,14 @@ import fri.shapesge.DataObrazku;
 
 public class Hrac {
     private Obrazok obrazokHraca;
+    int polohaHracaX = 250;
+    int polohaHracaY = 650;
 
     public Hrac(){
+
+        this.polohaHracaX = 250;
+        this.polohaHracaY = 650;
+
         this.obrazokHraca = new Obrazok("assets/hrac_RPG.png");
         obrazokHraca.zmenPolohu(250,650);
         obrazokHraca.zobraz();
@@ -20,21 +26,99 @@ public class Hrac {
 
     }
 
+    /*
+    * odstranit vypis v každej metode
+    * */
+
     public void posunVpravo() {
-        this.obrazokHraca.posunVodorovne(10);
+        if(getPolohaHracaX() == 470 ||
+                getPolohaHracaY() == 630 && getPolohaHracaX() == 390 ||
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 420 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 440 ||
+                getPolohaHracaY() == 660 && getPolohaHracaX() == 450 ||
+                getPolohaHracaY() == 670 && getPolohaHracaX() == 460
+
+        ){
+
+        } else {
+            this.obrazokHraca.posunVodorovne(10);
+            this.polohaHracaX += 10;
+            System.out.println("X:" + polohaHracaX);
+        }
     }
 
     public void posunVlavo() {
-        this.obrazokHraca.posunVodorovne(-10);
+        //Ošetrenie vypadnutia z lode
+        //Skusiť zjednodusit kod
+        if(getPolohaHracaX() == -10 ||
+                getPolohaHracaY() == 630 && getPolohaHracaX() == 50 ||
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 30 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 10 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 0
+        ){
+
+        } else{
+            this.obrazokHraca.posunVodorovne(-10);
+            this.polohaHracaX -= 10;
+            System.out.println("X:" + polohaHracaX);
+        }
+
     }
 
+
+
+    //Horna hranica lode je 640 Y
+
+
     public void posunHore() {
-        this.obrazokHraca.posunZvisle(-10);
+
+        if(polohaHracaY < 640 ||
+
+                //Lava
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 40 ||
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 30 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 20 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 10 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 0  ||
+                getPolohaHracaY() == 660 && getPolohaHracaX() == -10||
+
+                //Prava
+                 getPolohaHracaY() == 630 && getPolohaHracaX() == 390||
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 400 ||
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 410 ||
+                getPolohaHracaY() == 640 && getPolohaHracaX() == 420 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 430 ||
+                getPolohaHracaY() == 650 && getPolohaHracaX() == 440 ||
+                getPolohaHracaY() == 660 && getPolohaHracaX() == 450 ||
+                getPolohaHracaY() == 670 && getPolohaHracaX() == 460 ||
+                getPolohaHracaY() == 680 && getPolohaHracaX() == 470
+
+        ) {
+
+        }else {
+            this.obrazokHraca.posunZvisle(-10);
+            this.polohaHracaY -= 10;
+            System.out.println("Y:" + polohaHracaY);
+        }
     }
 
     public void posunDole() {
-        this.obrazokHraca.posunZvisle(+10);
+        //osetrenie aby hrac nepresiel za polku lode
+        if (getPolohaHracaY() != 680) {
+            this.obrazokHraca.posunZvisle(+10);
+            this.polohaHracaY += 10;
+            System.out.println("Y:" + polohaHracaY);
+        }
     }
+
+    public int getPolohaHracaX(){
+        return this.polohaHracaX;
+    }
+
+    public int getPolohaHracaY(){
+        return this.polohaHracaY;
+    }
+
 
 
 }
